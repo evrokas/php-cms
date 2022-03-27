7f0e55372e2ac1f24975f5d14bdbf1f7ac2ebdb0
Evangelos Rokas <evrokas@gmail.com>
2022-03-28 02:08:22 +0300

    added CDN for Fontawesome in includes/header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -8,6 +8,7 @@
   <title>Website Admin</title>
   
   <link href="styles.css" type="text/css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.5.0/css/all.min.css" integrity="sha512-QfDd74mlg8afgSqm3Vq2Q65e9b3xMhJB4GZ9OcHDVy1hZ6pqBJPWWnMsKDXM7NINoKqJANNGBuVRIpIJ5dogfA==" crossorigin="anonymous" referrerpolicy="no-referrer" />  
   
   <script src="https://cdn.ckeditor.com/ckeditor5/12.4.0/classic/ckeditor.js"></script>
   
